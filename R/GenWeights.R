36bfe93fab0f5d711deb81d439a3fb875432537f
mbsabath <bsabath1@gmail.com>
2017-11-01 16:58:38 -0400

    current R work

---
+++ R/GenWeights.R
@@ -0,0 +1,34 @@
+#' Generate geographic weighting matrix
+#'
+#' Given two sets of points, generate a matrix of weights for the k nearest neighbors
+#' for use in distance weighted averaging of values.
+#'
+#' @param orig the data in which the neighbors are being found
+#' @param query the data for which the neighbors need to be found
+#' @param k the number of neighbors being searched for
+#' @param threshold
+#'
+#' @import RANN
+#' @export
+gen_weights <- function(orig, query, k=4, threshold) {
+    neighbors <- nn2(data = orig, query = query, k = k)
+
+    weight_matrix <- process_weights(neighbors, nrow(orig), nrow(query), threshold)
+
+    return(weight_matrix)
+}
+
+
+process_weights <- function(neighbors, orig_len, query_len, threshold) {
+  out <- matrix( data = 0, nrow = query_len, ncol = orig_len)
+
+  for (i in 1:query_len) {
+    for (j in 1:nrow(neighbors$nn.idx)) {
+      if (neighbors$nn.dists[i,j] <= threshold && neighbors$nn.dists[i,j] != 0) {
+        out[i,neighbors$nn.idx[i,j]] <- 1/neighbors$nn.dists[i,j]
+      }
+    }
+  }
+
+  return(out)
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# NSAPH Air Pollution Prediction Code
+
+This project is a python implementation of Qian Di's air pollution prediction model.
+
+## Git Development Structure
+
+All new branches should be branched off of the `test` branch. Each branch off of that should be focused on implementing
+one specific feature for the overall program. Features should be developed and tested in their individual branches, then
+merged into `test` for integrated testing. If all merged features in `test` are functioning together, then `test` can be
+merged into `master`.
