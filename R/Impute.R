8d88f15b9ffbf50a1de88f4ba167e9575bdd8b8f
mbsabath <bsabath1@gmail.com>
2017-11-03 16:14:39 -0400

    generate weights with logit model

--- R/Impute.R
+++ R/Impute.R
@@ -1,11 +1,27 @@
 # Imputation Code
-#' @include utils.R
+
+#' Generate the weights for use in the mixed imputation model
 #'
+#' @param info a data.table or tibble with variables corresponding to those contained in the
+#'             yaml files in the package.
+#' @param var a string containing the name of the variable to be imputed
+#'
+#'
+#' @import dplyr
+#' @importFrom stats as.formula binomial glm predict.glm
+#' @include utils.R
+#' @export
+get_logit_weights <- function(info, var) {
 
-get_logit_weights <- function(data, var) {
 
+  info <- mutate(info, logit_var =is.na(info[[var]]))
   ## assemble formula
   covars <- get_formula(paste0(path.package("airpred"),"/yaml_files/logit_formula.yml"), var)
 
+  logit_model <- glm(as.formula(paste0("logit_var~",covars)),family = binomial(link = "logit"),
+                     data = info)
 
+  mle_weight <- 1/(1 - predict.glm(logit_model, type = "response"))
+
+  return(mle_weight)
 }
