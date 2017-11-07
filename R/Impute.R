103b834cf5e2d05d3e722e0cdc2a72b8df594063
mbsabath <bsabath1@gmail.com>
2017-11-06 22:15:49 -0500

    lme file work

--- R/Impute.R
+++ R/Impute.R
@@ -14,7 +14,7 @@
 get_logit_weights <- function(info, var) {
 
 
-  info <- mutate(info, logit_var =is.na(info[[var]]))
+  info <- mutate(info, logit_var = is.na(info[[var]]))
   ## assemble formula
   covars <- get_formula(paste0(path.package("airpred"),"/yaml_files/logit_formula.yml"), var)
 
@@ -23,5 +23,29 @@ get_logit_weights <- function(info, var) {
 
   mle_weight <- 1/(1 - predict.glm(logit_model, type = "response"))
 
+  info$logit_var <- NULL
   return(mle_weight)
+}
+
+#' Impute varibles using mixed linear models
+#'
+#' @param info data set
+#' @param var the variable to be imputed
+#'
+#' @return a vector of the imputed values
+#' @export
+#'
+#' @importFrom nlme lme
+#' @examples
+MLE_impute <- function(info, var) {
+  info$weights <- get_logit_weights(info, var)
+  covars <- get_formula(paste0(path.package("airpred"),"/yaml_files/lme_formula.yml"), var)
+  randoms <- get_formula(paste0(path.package("airpred"),"/yaml_files/lme_formula.yml"), "random")
+
+
+
+  m1.lme <- lme(as.formula(paste0(var,"~",covars)),info,
+                random = as.formula(paste0("~",randoms)), weights = ~weights)
+
+
 }
