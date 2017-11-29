50c5e024026815996bd3779d466a222355b1e81d
mbsabath <bsabath1@gmail.com>
2017-11-29 13:39:22 -0500

    Add config code, more dev on file processing

--- R/ReadData.R
+++ R/ReadData.R
@@ -18,6 +18,8 @@ Matlab2RDS <- function(path) {
 
 }
 
+
+
 process_annual <- function(){}
 
 process_daily <- function(){}
@@ -28,4 +30,66 @@ process_windspeed <- function(){}
 
 process_monitor <- function(){}
 
+process_data <- function(path = "../predictions/EPANO2") {
+  files <- gen_data_paths(path)
 
+  for (var in names(files)) {
+    if (var == "MonitorData") {
+      process_monitor()
+    } else if (substr(var,1,20) == "REANALYSIS_windspeed") {
+      process_windspeed()
+    } else if (length(files[[var]] == 1)) {
+      process_location()
+    } else if (readMat(files[[var]][1])$Result == 1) {
+      process_annual()
+    } else {
+      process_daily()
+    }
+  }
+}
+
+#' Generate Data File path
+#'
+#' @param path the directory storing the data to be used in the model
+#'
+#' @return
+#' A list where each object is a variable containing a vector of all existing files
+#' in the checked directory that match the file name pattern for that variable specified in
+#' the Data_Location.yml file.
+#'
+#'
+#'
+#'
+gen_data_paths <- function(path = "../predictions/EPANO2") {
+
+  # Initialize List
+  file.yaml <- list()
+
+  # get names of variables in dataset
+  varlist <- yaml.load_file(file.path(path.package("airpred"),"yaml_files",
+                                      "Data_Location.yml"))
+
+
+  for (variable in names(varlist)) {
+    if (length(varlist[[variable]]) == 2) {
+      directory <- varlist[[variable]][1]
+      files <- file.path(directory,list.files(file.path(path, directory),
+                                              pattern = varlist[[variable]][2]))
+    } else {
+      directory <- varlist[[variable]][2]
+      files <- file.path(directory,list.files(file.path(path, directory),
+                                              pattern = varlist[[variable]][3]))
+    }
+    if (length(files) > 0) {
+      listname <- variable
+      # while (!is.null(file.yaml[[listname]])){
+      #   listname <- paste0(listname, "bad")
+      # }
+      file.yaml[[listname]] <- files
+      #} else {
+      #  file.yaml[[variable]] <- c("bad", directory)
+    }
+  }
+
+  return(file.yaml)
+}
--- R/utils.R
+++ R/utils.R
@@ -31,4 +31,28 @@ get_formula<- function(path, var="base") {
 }
 
 
+#' Generate Config File Skeleton
+#'
+#' @param path the directory that the config file should be saved in
+#'
+#' @return Null, but saves a yml file with the headers needed to run the prediction model saved
+#' @export
+#'
+#' @details
+#' \itemize{
+#'   \item{\code{Monitor}} {The pollution type the data will be trained on}
+#'   \item{\code{Data_Location}} {The directory holding the required data files}
+#'   \item{\code{train}} {A boolean. If TRUE, the model run is a training run. If false,
+#'                        the run is going to be used to create predictions}
+#'  }
+gen_config <- function(path = ".") {
+  out <- list()
+  out$Monitor <- ""
+  out$Data_Location <- ""
+  out$train <- FALSE
 
+  out.file <- file(file.path(path, "config.yml"))
+  write(as.yaml(out), file=out.file)
+  close(out.file)
+
+}
