3b0e3cba6d69ace7c039036363696daef67e7a55
mbsabath <bsabath1@gmail.com>
2018-01-19 14:54:27 -0500

    fix function bug

--- R/ReadData.R
+++ R/ReadData.R
@@ -338,5 +338,5 @@ join_data <- function(files = NULL) {
   }
   saveRDS(out, file = file.path(save_path, "assembled_data.RDS"))
 
-  write.CSV(out, file = file.path(save_path, "assembled_data.csv"))
+  write.csv(out, file = file.path(save_path, "assembled_data.csv"))
 }
